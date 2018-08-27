fdc4c33e9d96793f478ab0178781214331c84826
Nickghost inc <you@example.com>
2018-08-27 12:57:33 +0300

    views.py file updated

--- app/views.py
+++ app/views.py
@@ -10,8 +10,8 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
@@ -20,3 +20,6 @@ def movie(movie_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('movie.html',id = movie_id)    
+
+
+    
