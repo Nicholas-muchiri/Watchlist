9373b05cabfde241a29f9cf0895a4709a2ba196f
Nickghost inc <you@example.com>
2018-08-28 16:14:48 +0300

    reviews.py file created

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from app import app
 from .request import get_movies,get_movie,search_movie
 
 
-    # Views
+ # Views
 @app.route('/')
 def index():
 
@@ -15,8 +15,16 @@ def index():
     popular_movies = get_movies('popular')
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
+
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
+        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
 
 @app.route('/movie/<int:id>')
 def movie(id):
