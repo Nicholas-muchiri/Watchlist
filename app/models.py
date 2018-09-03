99a4a67b25ee1455fff6b3290255122a1708e56d
Nickghost inc <you@example.com>
2018-09-03 17:07:35 +0300

    models.py updated

--- app/models.py
+++ app/models.py
@@ -2,8 +2,10 @@ from . import db
 
 class User(db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.Foreignkey("roles.id"))
 
     def __repr__(self):
         return f'User {self.username}'
